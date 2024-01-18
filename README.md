d8148694e3c9e88720b181596e9a214487870801
54younger <1831945484@qq.com>
2024-01-18 16:46:48 +0800

    optimise file structure

---
+++ README.md
@@ -0,0 +1,22 @@
+# 功能介绍
+-- blender.py 将专辑封面材质绑定至黑胶唱片,并将唱片机模型导出为.glb
+-- image_processing.py 改变专辑封面大小,使其贴合黑胶唱片(因为我也不知道怎么改材质大小,用一个笨方法)
+-- *.blend 模型,未添加专辑封面
+-- img 专辑封面文件夹
+  -- resize 处理后的专辑封面文件夹
+-- temp.blend 添加封面时临时文件,删除blender.py文件最后一行代码可保留
+-- *.glb 最终成品
+
+# 使用方法
+## 1
+先运行image_process.py,得到处理后的专辑封面.
+*注意更改代码内路径*
+
+## 2
+  #### 运行blender.py命令为:
+  你的blender安装路径(不要有空格)(这个应该不用强调吧)/blender.exe -b -P *该文件夹绝对路径*/blender.py -- --parent_dir *该文件夹绝对路径* --pic_name *专辑封面名(只有名字,无需要加后缀)* --blender_file *blender_file.blend*
+
+*example*: blender.exe -b -P C:/Users/younger/Desktop/output_model_auto/blender.py -- --parent_dir C:/Users/younger/Desktop/output_model_auto --pic_name 2 --blender_file red_card.blend
+
+## 3
+得到目标文件
---
+++ blender.py
@@ -0,0 +1,73 @@
+import bpy
+import os
+import sys
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--parent_dir", type = str)
+    parser.add_argument("--pic_name", type = str)
+    parser.add_argument("--blender_file", type = str)
+    argv = sys.argv[sys.argv.index("--") + 1 :]
+    return parser.parse_args(argv)
+
+if __name__ == "__main__":
+
+    args=parse_args()
+    parent_dir = args.parent_dir
+    blender_file = os.path.join(parent_dir, args.blender_file)
+
+    if not os.path.exists(blender_file):
+        print("blender file not exist")
+        exit(1)
+
+    temp_file = os.path.join(parent_dir, "temp.blend")
+    temp_file1= os.path.join(parent_dir, "temp.blend1")
+    
+    pic_path = os.path.join(parent_dir, "img", "resize", args.pic_name + "_resize.jpg")
+    model_file = os.path.join(parent_dir, args.blender_file.split(".")[0] + ".glb")
+
+    bpy.ops.wm.open_mainfile(filepath=blender_file)
+    bpy.ops.wm.save_as_mainfile(filepath=temp_file)
+    try:
+      obj = bpy.data.objects["pointer"]
+      obj.select_set(True)
+      obj.rotation_mode = 'QUATERNION'
+      bpy.ops.object.select_all(action='DESELECT')
+    except:
+        pass
+
+
+    obj = bpy.data.objects["plain"]
+    obj.select_set(True)
+    bpy.context.view_layer.objects.active = obj
+
+    # 添加材质
+    mat = bpy.data.materials.new(name="Material")
+    obj.data.materials.append(mat)
+    mat.use_nodes = True
+    nodes = mat.node_tree.nodes
+    links = mat.node_tree.links
+
+    # 添加原理化BSDF节点
+    bsdf_node = None
+    for node in nodes:
+        if node.type == 'BSDF_PRINCIPLED':
+            bsdf_node = node
+            break
+    bsdf_node.location = (0, 0)
+    # 添加图像纹理节点
+    tex_node = nodes.new(type='ShaderNodeTexImage')
+    tex_node.location = (-400, 0)
+    tex_node.image = bpy.data.images.load(filepath=pic_path)
+    # 连接图像纹理节点和原理化BSDF节点
+    links.new(tex_node.outputs[0], bsdf_node.inputs["Base Color"])
+    bpy.ops.wm.save_as_mainfile(filepath=temp_file)
+
+    # 导出模型为.glb文件
+    bpy.ops.export_scene.gltf(filepath=model_file, export_animations=True, export_animation_mode='ACTIVE_ACTIONS')
+
+    bpy.ops.wm.quit_blender()
+
+    os.remove(temp_file)
+    os.remove(temp_file1)
